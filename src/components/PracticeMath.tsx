1939a97b7b90428228dc600dadd3501bca56ca71
AVINASH KUMAR SINGH <109014651+AcAvinash@users.noreply.github.com>
2023-04-23 23:50:26 +0530

    Add files via upload

---
+++ src/components/PracticeMath.tsx
@@ -0,0 +1,32 @@
+
+const PracticeMath = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <h1
+              className="heading-text mt-5"
+            >
+              Practicing Math was never this easy
+            </h1>
+            <p className="headingPara">
+              Solve NCERT math sums in a notebook like <br /> fashion. Get step-by-step
+              feedback instantly, <br /> track your progress, and much more.{" "}
+            </p>
+            <button className="contact math">
+              Get started
+            </button>
+          </div>
+          <div className="col-md-6">
+            <div
+              className="videoContainer">
+              
+            </div> 
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default PracticeMath;
